7ec39809f306a7bb504a5df480c17f2001eb38a1
shuning zhou <shuningzhou@peterzhou.local>
2015-01-28 07:57:33 -0800

    Initial Commit

---
+++ JS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JS
+//
+//  Created by shuning zhou on 2015-01-28.
+//  Copyright (c) 2015 Intrahealth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JS
+//
+//  Created by shuning zhou on 2015-01-28.
+//  Copyright (c) 2015 Intrahealth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
